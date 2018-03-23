96dd17ff615c9670681b5350a712668f58f24827
s0545820 <s0545820@gmx.de>
2018-03-23 12:34:32 +0100

    added ban and show banned users functionality

--- routes/users.js
+++ routes/users.js
@@ -128,25 +128,26 @@ router.post('/unban', function(req,res) {
 
 });
 router.get('/banned', function(req,res) {
-  var token = req.headers['x-access-token'];
+  res.send('hi');
+  /*var token = req.headers['x-access-token'];
   if (!token) res.status(401).json({message:'Not authenticated. Please log in.'});
   jwt.verify(token, process.env.SECRET, function (err, decoded) {
     if(err) res.status(500).json({message: err.message});
     if(decoded.role == 'admin') {
-    /*  User.find({banned: true}, function(err, users) {
+      User.find({banned: true}, function(err, users) {
         if (err)
             res.status(500).json({message: err.message});
         if (!users)
             res.status(401).json({message: 'No banned users.'});
         res.status(200).json({users: users});
-      });*/
+      });
       var users = [
         {username: 'max'},
         {username: 'mex'}
       ]
       res.status(200).json({users: users});
     }
-  });
+  });*/
 });
 
 module.exports = router;
