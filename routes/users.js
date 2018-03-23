2500b432654883deb4e934d242592f70b4f7ccb8
s0545820 <s0545820@gmx.de>
2018-03-23 14:14:34 +0100

    .

--- routes/users.js
+++ routes/users.js
@@ -125,30 +125,35 @@ router.post('/ban', function(req,res) {
   });
 });
 router.post('/unban', function(req,res) {
-
-});
-router.get('/banned', function(req,res) {
-  var banned_users = [];
   var token = req.headers['x-access-token'];
   if (!token) res.status(401).json({message:'Not authenticated. Please log in.'});
   jwt.verify(token, process.env.SECRET, function (err, decoded) {
     if(err) res.status(500).json({message: err.message});
-    if(decoded.role == 'admin') {
-      User.find({}, function(err, users) {
-        if (err)
-            res.status(500).json({message: err.message});
-        if (!users) {
-            res.status(401).json({message: 'No banned users.'});
-        } else {
-          for(var i=0;i<users.length;i++) {
-            if(users[i].banned) {
-              banned_users.push(users[i]);
+    User.findOne({_id: decoded.user_id}, {password: 0}, function(err, user) {
+      if (err)
+          res.status(500).json({message: err.message});
+      if (!user) {
+        res.status(404).json({message: 'No User found.'});
+      } else {
+        if(user.role === 'admin') {
+          User.findOne({_id: req.body.userid}, {password: 0}, function(err, user_to_ban) {
+            if (err)
+                res.status(500).json({message: err.message});
+            if (!user_to_ban) {
+              res.status(404).json({message: 'No User found.'});
+            } else {
+              user_to_ban.banned = false;
+              user_to_ban.save(function(err) {
+                if (err) res.status(500).json({message: err.message});
+                res.status(200).json({success: true, message: 'User was successfully unbanned.'});
+              });
             }
-          }
-          res.status(200).json({users: banned_users});
+          });
+        } else {
+          res.status(501).json({message: 'You are not allowed to unban users.'});
         }
-      });
-    }
+      }
+    });
   });
 });
 
